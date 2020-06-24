d7905a34d1a8f711634f5052d5491cca2829a6bb
Menglin Chen <41313150+ChanMenglin@users.noreply.github.com>
2020-06-24 15:14:21 +0800

    edit git config core.ignorecase false

---
+++ src/components/base/Bootstrap/Form/Basic/basic-textarea.vue
@@ -0,0 +1,43 @@
+<template>
+  <textarea
+    :class="[cClass, readonlyClass]"
+    :style="cStyle"
+    :rows="Number(rows)"
+    :cols="Number(cols)"
+    :readonly="readonly"
+    :aria-readonly="readonly"
+    :disabled="disabled"
+    :aria-disabled="disabled"
+    v-bind="$attrs"
+    v-on="inputListeners"
+  />
+</template>
+<script>
+import util from "@/components/util/index.js";
+
+export default {
+  name: "basic-textarea",
+  mixins: [
+    util.mixins.form.base,
+    util.mixins.form.readonly,
+  ],
+  inheritAttrs: false,
+  props: {
+    rows: {
+      ...util.props.UInt,
+      default: 3
+    },
+    cols: util.props.UInt,
+    maxlength: {
+      ...util.props.UInt,
+      default: 1024
+    },
+    resize: util.props.Boolean
+  },
+  computed: {
+    cStyle: function() {
+      return this.resize ? "" : "resize: none";
+    }
+  },
+};
+</script>
