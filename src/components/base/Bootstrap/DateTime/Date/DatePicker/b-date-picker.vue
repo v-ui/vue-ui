b60adbdf1ac1e7652f771e9f75fb4458b0362202
Menglin Chen <41313150+ChanMenglin@users.noreply.github.com>
2020-09-03 10:24:24 +0800

    use [b-date-panel]

--- src/components/base/Bootstrap/DateTime/Date/DatePicker/b-date-picker.vue
+++ src/components/base/Bootstrap/DateTime/Date/DatePicker/b-date-picker.vue
@@ -10,33 +10,13 @@
       <i class="far fa-calendar-alt" />
     </template>
     <template>
-      <div class="d-flex">
-        <date-panel-select
-          v-model="selectedValueStart"
-          class="flex-fill"
-          :type="type"
-          :min="dateMin"
-          :max="range ? rangeStartMax : dateMax"
-          :disabled="disabled"
-          :range="range"
-          :selectedStart="selectedValueStart"
-          :selectedEnd="selectedValueEnd"
-          :hide-header="false"
-        />
-        <date-panel-select
-          v-if="range"
-          v-model="selectedValueEnd"
-          class="flex-fill"
-          :type="type"
-          :min="range ? rangeEndMin : dateMin"
-          :max="dateMax"
-          :disabled="disabled"
-          :range="range"
-          :selectedStart="selectedValueStart"
-          :selectedEnd="selectedValueEnd"
-          :hide-header="false"
-        />
-      </div>
+      <b-date-panel
+        v-model="selectedValues"
+        :type="pickertType"
+        :min="dateMin"
+        :max="dateMax"
+        :range="range"
+      />
     </template>
   </dropdown-panel>
 </template>
@@ -45,12 +25,12 @@
 import config from '@/config/index.js'
 import util from "@/components/util/index.js";
 
-import datePanelSelect from './Basic/date-panel-select'
+import BDatePanel from '@/components/base/Bootstrap/DateTime/Date/DatePicker/b-date-panel.vue'
 import dropdownPanel from "@/components/base/Bootstrap/DropdownPanel/b-dropdown-panel.vue";
 
 export default {
   name: "b-date-picker",
-  components: { datePanelSelect, dropdownPanel, },
+  components: { BDatePanel, dropdownPanel, },
   mixins: [
     util.mixins.form.base,
     util.mixins.moment.base,
@@ -79,14 +59,8 @@ export default {
   },
   data() {
     return {
-      pickertType: "",
-      // 默认使用 selectedValueStart，
-      // 当 range 为 True 时才使用 selectedValueEnd，
-      // 此时 selectedValueStart 相当于 start，selectedValueEnd 相当于 end
-      selectedValueStart: null,
-      selectedValueEnd: null,
-      selectedValues: this.range ? {start: null, end: null} : null,
-
+      pickertType: this.type,
+      selectedValues: this.value,
     };
   },
   computed: {
@@ -124,40 +98,6 @@ export default {
     dateMax: function () {
       return this.moment(new Date(this.max));
     },
-    rangeStartMax: function() {
-      if (this.selectedValueEnd && this.selectedValueEnd.isValid && this.selectedValueEnd.isValid()) {
-        let end = this.selectedValueEnd.clone()
-        switch (this.type) {
-          case this.enumTypeStatus.year:
-          return end.subtract(1, 'y')
-        case this.enumTypeStatus.month:
-          return end.subtract(1, 'm')
-        case this.enumTypeStatus.date:
-          return end.subtract(1, 'd')
-        default:
-          return end
-        }
-      } else {
-        return this.dateMax
-      }
-    },
-    rangeEndMin: function() {
-      if (this.selectedValueStart && this.selectedValueStart.isValid && this.selectedValueStart.isValid()) {
-        let start = this.selectedValueStart.clone()
-        switch (this.type) {
-          case this.enumTypeStatus.year:
-          return start.add(1, 'y')
-        case this.enumTypeStatus.month:
-          return start.add(1, 'm')
-        case this.enumTypeStatus.date:
-          return start.add(1, 'd')
-        default:
-          return start
-        }
-      } else {
-        return this.dateMin
-      }
-    },
     fillInfo: function () {
       let minIsValid = this.dateMin.isValid()
       let maxIsValid = this.dateMax.isValid()
@@ -179,13 +119,11 @@ export default {
     },
   },
   watch: {
-    selectedValueStart: function (value) {
-      this.range
-        ? this.selectedValues.start = value
-        : this.selectedValues = value
-    },
-    selectedValueEnd: function (value) {
-      this.selectedValues.end = value
+    value: {
+      handler: function (value) {
+        this.selectedValues = value
+      },
+      deep: true,
     },
     selectedValues: {
       handler: function (value) {
@@ -194,24 +132,6 @@ export default {
       },
       deep: true,
     },
-  },
-  mounted() {
-    this.pickertType = this.type;
-
-    if (this.range) {
-      let date = {}
-      date.start = this.moment(this.value.start);
-      date.end = this.moment(this.value.end);
-      date.start = date.start.isValid() ? date.start : this.moment();
-      date.end = date.end.isValid() ? date.end : this.moment();
-      this.selectedValueStart = date.start
-      this.selectedValueEnd = date.end
-    } else {
-      let date = this.moment(this.value);
-      date = date.isValid() ? date : this.moment();
-      this.selectedValueStart = date
-    }
-
   },
   methods: {
     formatDate: function (value) {
