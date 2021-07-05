3457e191f92e7dace4362e68cbef5b94adc2f26f
Menglin Chen <41313150+ChanMenglin@users.noreply.github.com>
2021-07-05 16:38:05 +0800

    edit [b-list] and add [b-check-list]

---
+++ src/components/base/Grid/b-grid.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-grid-row
+    v-if="type === row"
+  >
+    <template #item={ item, disabled }>
+      <slot name="item" :item="item" :disabled="disabled" />
+    </template>
+  </b-grid-row>
+  <b-grid-row
+    v-else
+  >
+    <template #item={ item, disabled }>
+      <slot name="item" :item="item" :disabled="disabled" />
+    </template>
+  </b-grid-row>
+</template>
+
+<script>
+import util from "@/components/util/index.js";
+
+import BGridRow from './b-grid-row.vue'
+import BGridTable from './b-grid-table.vue'
+export default {
+  name: 'b-grid',
+  components: { BGridRow, BGridTable, },
+  mixins: [ util.mixins.select.select, ],
+  props: {
+    type: {
+      type: String,
+      default: 'row',
+      validator: value => ['row', 'table'].includes(value),
+    },
+    colCount: util.props.UInt,
+    border: util.props.Boolean,
+    disabled: util.props.Boolean,
+  },
+}
+</script>
