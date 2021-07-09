8ed22d50f45bfe93a3fe4e2d61523f0a624c689d
Menglin Chen <41313150+ChanMenglin@users.noreply.github.com>
2021-07-09 15:20:11 +0800

    edit [basic-info]

--- src/components/basic/basic-info.vue
+++ src/components/basic/basic-info.vue
@@ -13,10 +13,7 @@ export default {
   name: "BasicInfo",
   props: {
     info: util.props.String,
-    infoColor: {
-      ...util.props.textColor,
-      default: "muted"
-    }
+    infoColor: util.props.textColor,
   }
 };
 </script>
